ae8bb7ef8c2c2a115008f779e4744f927dd14580
Muhammad Athar <77805224+AtharSial0306@users.noreply.github.com>
2024-05-10 20:39:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
     // currentScore = scores[activePlayer];
     // Check teh Player score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       //Finish the Game
       playing = false;
       diceEl.classList.add('hidden');
